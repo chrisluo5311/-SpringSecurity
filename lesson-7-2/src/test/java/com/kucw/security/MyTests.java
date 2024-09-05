dd41c439cbf1d750f084de0bd38be2e0e1c59335
chrisluo5311 <chrislo5311@gmail.com>
2024-09-05 16:18:33 +0800

    add spring security test
    1. with (http Basic(username, password))
    2. 使用@WithMockUser

--- lesson-7-2/src/test/java/com/kucw/security/MyTests.java
+++ lesson-7-2/src/test/java/com/kucw/security/MyTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,8 +22,60 @@ public class MyTests {
     @Test
     public void testHello() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/hello");
+                .get("/hello")
+                        .header("Authorization","Basic dGVzdDFAZ21haWwuY29tOjExMQ==");
 
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void testHello2() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/hello")
+                .with(httpBasic("test1@gmail.com", "111"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void testWelcome() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/welcome")
+                .with(httpBasic("test1@gmail.com", "111"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void testWelcome2() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/welcome")
+                .with(httpBasic("test2@gmail.com", "222"));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(403));
+    }
+
+    //@WithMockUser是創建一個假的user進行SpringSecurity的測試
+    //這個假的User的帳號是mock，擁有Role是NORMAL_MEMBER
+    //比較推薦根據資料庫的資料測試，因為@WithMockUser是創建假的user
+    @WithMockUser(username = "mock",roles = {"NORMAL_MEMBER"})
+    @Test
+    public void testWelcome3() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/welcome");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(403));
+    }
+
+    @WithMockUser(username = "mock2",roles = {"ADMIN"})
+    @Test
+    public void testWelcome4() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/welcome");
         mockMvc.perform(requestBuilder)
                 .andExpect(status().is(200));
     }
